76e7fdac69416d976c4e3d5128a1ecfe160867b7
Francisco Escobar <escobarefrancisco@gmail.com>
2023-05-09 20:27:41 -0300

    continuamos creando

---
+++ src/components/ItemLista.js
@@ -0,0 +1,13 @@
+
+function ItemLista (){
+
+    return(
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+    )
+}
+export default ItemLista
---
+++ src/pages/admin.js
@@ -0,0 +1,15 @@
+import Table from 'react-bootstrap/Table';
+import ItemLista from '../components/ItemLista';
+
+
+
+function Hola(){
+
+
+    return(
+        <>
+         
+        </>
+    )
+}
+export default Hola
