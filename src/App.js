c4f2018a0e4693d65125d101ddda2371331a765f
Juan Pablo Moya <juanpablo785@hotmail.com>
2023-05-22 19:44:48 -0300

    pagina 404 y pagina acerca de nosotros

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ import Comentario from './pages/agregarComentario';
 import AdminDetallesPlanes from './pages/adminDetallesPlanes';
 import AgregarPlan from './pages/agregarPlan';
 import EditarPlan from './pages/editarPlan';
-
+import Error from './pages/error';
+import AcercaDeNosotros from './pages/acercaDeNosotros';
 
 function App() {
   return (
@@ -38,6 +39,8 @@ function App() {
     <Route path='/adminDetallesPlanes' element={<AdminDetallesPlanes/>} ></Route>
     <Route path='/agregarPlan' element={<AgregarPlan/>} ></Route>
     <Route path='/editarPlan/:_id' element={<EditarPlan/>} ></Route>
+    <Route path='/acercaDeNosotros' element={<AcercaDeNosotros/>} ></Route>
+    <Route path='/*' element={<Error/>} ></Route>
 
 
     </Routes>
