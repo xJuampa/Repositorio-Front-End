6a8211231d89600bf8dec618ce9a41f4021fc199
Juan Pablo Moya <juanpablo785@hotmail.com>
2023-11-14 22:24:22 -0300

    Cambios en el Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,22 +8,22 @@ function Footer(){
         <MDBFooter bgColor='dark' className='text-center text-lg-start text-muted'>
       <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
         <div>
-          <a href='' className='me-4 text-reset'>
+          <a href='#' className='me-4 text-reset'>
             <MDBIcon fab icon="facebook-f" />
           </a>
-          <a href='' className='me-4 text-reset'>
+          <a href='#' className='me-4 text-reset'>
             <MDBIcon fab icon="twitter" />
           </a>
-          <a href='' className='me-4 text-reset'>
+          <a href='#' className='me-4 text-reset'>
             <MDBIcon fab icon="google" />
           </a>
-          <a href='' className='me-4 text-reset'>
+          <a href='#' className='me-4 text-reset'>
             <MDBIcon fab icon="instagram" />
           </a>
-          <a href='' className='me-4 text-reset'>
+          <a href='#' className='me-4 text-reset'>
             <MDBIcon fab icon="linkedin" />
           </a>
-          <a href='' className='me-4 text-reset'>
+          <a href='#' className='me-4 text-reset'>
             <MDBIcon fab icon="github" />
           </a>
         </div>
